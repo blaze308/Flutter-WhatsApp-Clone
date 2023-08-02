088892035b685b921e93e3372ef5f5749e1c9c81
blaze308 <jaimepinkrah@gmail.com>
2023-08-02 12:42:39 +0000

    new

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/widgets/chat_list.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
 import 'package:whatsapp_clone/widgets/web_chat_appbar.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
@@ -34,7 +36,56 @@ class WebScreenLayout extends StatelessWidget {
             ),
             child: Column(
               children: [
-                WebChatAppBar(),
+                const WebChatAppBar(),
+                const Expanded(child: ChatList()),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.09,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(color: dividerColor)),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.emoji_emotions_outlined,
+                            color: Colors.grey,
+                          )),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.attach_file,
+                            color: Colors.grey,
+                          )),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 15),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                fillColor: searchBarColor,
+                                filled: true,
+                                hintText: "Type a message...",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: const BorderSide(
+                                      width: 0, style: BorderStyle.none),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(left: 20)),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.mic,
+                            color: Colors.grey,
+                          )),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
--- lib/widgets/web_chat_appbar.dart
+++ lib/widgets/web_chat_appbar.dart
@@ -15,13 +15,30 @@ class WebChatAppBar extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       color: webAppBarColor,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          CircleAvatar(backgroundImage: NetworkImage(url), radius: 30),
-          SizedBox(width: MediaQuery.of(context).size.width * 0.01),
-          Text(
-            info[0]["name"].toString(),
-            style: const TextStyle(fontSize: 18),
+          Row(
+            children: [
+              CircleAvatar(backgroundImage: NetworkImage(url), radius: 30),
+              SizedBox(width: MediaQuery.of(context).size.width * 0.01),
+              Text(
+                info[0]["name"].toString(),
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
           ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search, color: Colors.grey),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert, color: Colors.grey),
+              ),
+            ],
+          )
         ],
       ),
     );
